f87498563b953a902757ac32aef828e618e107cf
Edu <eduardo.caro.lorente@gmail.com>
2024-02-19 20:09:34 +0100

    métodos alumno

--- Alumno.java
+++ Alumno.java
@@ -3,4 +3,22 @@ public class Alumno {
     int edad;
     int curso;
     String nombre;
+    Alumno(){
+        this.num = 0;
+        this.edad = 12;
+        this.curso = 1;
+        this.nombre = "";
+    }
+    Alumno(int num, int edad, int curso, String nombre){
+        this.num  = num;
+        this.edad = edad;
+        this.curso = curso;
+        this.nombre = nombre;
+    }
+    Alumno (Alumno original){
+        this.num = original.num;
+        this.edad = original.edad;
+        this.curso = original.curso;
+        this.nombre = original.nombre;
+    }
 }
